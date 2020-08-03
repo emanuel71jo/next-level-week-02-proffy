5108f8655b85408358a97e2e0d2fb9d5b2e3f7cc
emanuel71jo <emanuel71jo@gmail.com>
2020-08-03 20:20:25 -0300

    Estilos da pagina TeacherList

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,9 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-// import { Container } from './styles';
+import backIcon from "../../assets/images/icons/back.svg";
+import logoIcon from "../../assets/images/logo.svg";
+
+import "./styles.css";
 
 const TeacherList: React.FC = () => {
-  return <h1>Teacher List</h1>;
+  return (
+    <div id="page-teacher-list" className="container">
+      <header className="page-header">
+        <div className="top-bar-container">
+          <Link to="/">
+            <img src={backIcon} alt="Voltar" />
+          </Link>
+          <img src={logoIcon} alt="Proffy" />
+        </div>
+        <div className="header-content">
+          <strong>Estes são os proffys disponíveis</strong>
+        </div>
+      </header>
+    </div>
+  );
 };
 
 export default TeacherList;
