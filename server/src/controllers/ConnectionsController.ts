1ca6033ce018f287b3ac8da6e65414d10e730fc7
emanuel71jo <emanuel71jo@gmail.com>
2020-08-04 21:56:04 -0300

    Rota para listar o total de conexões

--- server/src/controllers/ConnectionsController.ts
+++ server/src/controllers/ConnectionsController.ts
@@ -2,7 +2,13 @@ import { Request, Response } from "express";
 import db from "../database/connection";
 
 export default class ConnectionsController {
-  async index(req: Request, res: Response) {}
+  async index(req: Request, res: Response) {
+    const totalConnections = await db("connection").count("* as total");
+
+    const { total } = totalConnections[0];
+
+    return res.json({ total });
+  }
 
   async create(req: Request, res: Response) {
     const { user_id } = req.body;
