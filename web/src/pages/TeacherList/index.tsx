a68b7eb3b0c1341755e8681ffc1e3ce5fbcd4092
emanuel71jo <emanuel71jo@gmail.com>
2020-08-05 21:52:27 -0300

    Adicionado o select no TeacherList

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -4,14 +4,41 @@ import PageHeader from "../../components/PageHeader";
 import TeacherItem from "../../components/TeacherItem";
 import Input from "../../components/Input";
 import "./styles.css";
+import Select from "../../components/Select";
 
 const TeacherList: React.FC = () => {
   return (
     <div id="page-teacher-list" className="container">
       <PageHeader title="Estes são os proffys disponíveis">
         <form id="search-teachers">
-          <Input label="Matéria" name="subject" type="text" />
-          <Input label="Dia da semana" name="week-day" type="text" />
+          <Select
+            label="Matéria"
+            name="subject"
+            options={[
+              { value: "Artes", label: "Artes" },
+              { value: "Geografia", label: "Geografia" },
+              { value: "Matemática", label: "Matemática" },
+              { value: "Educação Física", label: "Educação Física" },
+              { value: "Física", label: "Física" },
+              { value: "Ciência", label: "Ciência" },
+              { value: "Química", label: "Química" },
+              { value: "História", label: "História" },
+              { value: "Português", label: "Português" },
+            ]}
+          />
+          <Select
+            label="Dia da semana"
+            name="week-day"
+            options={[
+              { value: "0", label: "Domingo" },
+              { value: "1", label: "Segunda-feira" },
+              { value: "2", label: "Terça-feira" },
+              { value: "3", label: "Quarta-feira" },
+              { value: "4", label: "Quinta-feira" },
+              { value: "5", label: "Sexta-feira" },
+              { value: "6", label: "Sábado" },
+            ]}
+          />
           <Input label="Hora" name="time" type="time" />
         </form>
       </PageHeader>
