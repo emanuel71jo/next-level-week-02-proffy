a3753347a1af55761015599b8b6f244ac434a717
emanuel71jo <emanuel71jo@gmail.com>
2020-08-05 21:02:25 -0300

    Adição do component de Input

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,28 +1,18 @@
 import React from "react";
 
-import "./styles.css";
 import PageHeader from "../../components/PageHeader";
 import TeacherItem from "../../components/TeacherItem";
+import Input from "../../components/Input";
+import "./styles.css";
 
 const TeacherList: React.FC = () => {
   return (
     <div id="page-teacher-list" className="container">
       <PageHeader title="Estes são os proffys disponíveis">
         <form id="search-teachers">
-          <div className="input-block">
-            <label htmlFor="subject">Matéria</label>
-            <input type="text" id="subject" />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="week-day">Dia da semana</label>
-            <input type="text" id="week-day" />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="time">Hora</label>
-            <input type="text" id="time" />
-          </div>
+          <Input label="Matéria" name="subject" type="text" />
+          <Input label="Dia da semana" name="week-day" type="text" />
+          <Input label="Hora" name="time" type="time" />
         </form>
       </PageHeader>
 
