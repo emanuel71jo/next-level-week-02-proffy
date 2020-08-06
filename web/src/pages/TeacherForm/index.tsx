c8eaf179d6e77dfc62125473fd84ab56f7fdfa62
emanuel71jo <emanuel71jo@gmail.com>
2020-08-05 21:02:38 -0300

    Adição do component de Input

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,10 +1,26 @@
 import React from "react";
 import PageHeader from "../../components/PageHeader";
 
+import "./styles.css";
+import Input from "../../components/Input";
+
 const TeacherForm: React.FC = () => {
   return (
-    <div id="page-teacher-list" className="container">
-      <PageHeader title="Que incrível que você quer dar aula" />
+    <div id="page-teacher-form" className="container">
+      <PageHeader
+        title="Que incrível que você quer dar aula"
+        description="O primeiro passo é preencher esse formulário de inscrição"
+      />
+
+      <main>
+        <fieldset>
+          <legend>Seus dados</legend>
+
+          <Input label="Nome completo" name="name" type="text" />
+          <Input label="Avatar" name="avatar" type="text" />
+          <Input label="Whatsapp" name="whatsapp" type="text" />
+        </fieldset>
+      </main>
     </div>
   );
 };
