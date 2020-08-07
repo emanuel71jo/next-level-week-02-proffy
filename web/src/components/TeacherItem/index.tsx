3e2b9a3d4409b8e8020545fbc410b7755ee1cdf2
emanuel71jo <emanuel71jo@gmail.com>
2020-08-06 21:34:04 -0300

    Substituição dos item staticos do teacherItem

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -5,34 +5,35 @@ import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
 
 import "./styles.css";
 
-const TeacherItem: React.FC = () => {
+interface TeacherItemsProps {
+  teacher: {
+    avatar: string;
+    name: string;
+    bio: string;
+    cost: number;
+    id: number;
+    subject: string;
+    whatsapp: string;
+  };
+}
+
+const TeacherItem: React.FC<TeacherItemsProps> = ({ teacher }) => {
   return (
     <article className="teacher-item">
       <header>
-        <img
-          src="https://avatars1.githubusercontent.com/u/38570356?s=460&u=1fea95bee8f26c8ae24bdc8675168700c7d2b242&v=4"
-          alt="João Emanuel"
-        />
+        <img src={teacher.avatar} alt={teacher.name} />
         <div>
-          <strong>João Emanuel</strong>
-          <span>Química</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
 
-      <p>
-        Entusiasta das melhores tecnologias de química avançada.
-        <br />
-        <br />
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis eos
-        quaerat nostrum quos, aut sequi quisquam, iste perferendis consequatur
-        reiciendis dolores dicta? Animi consequuntur commodi doloribus sit saepe
-        officiis repellendus.
-      </p>
+      <p>{teacher.bio}</p>
 
       <footer>
         <p>
           Preço/hora
-          <strong>R$ 80,00</strong>
+          <strong>R$ {teacher.cost}</strong>
         </p>
 
         <button type="button">
