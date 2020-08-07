9ea283ec682e183b2dd243973b73d730cc20de44
emanuel71jo <emanuel71jo@gmail.com>
2020-08-07 00:31:49 -0300

    Fim do page header

---
+++ mobile/src/components/PageHeader/index.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { View, Image, Text } from "react-native";
+import { BorderlessButton } from "react-native-gesture-handler";
+
+import styles from "./styles";
+
+import backIcon from "../../assets/images/icons/back.png";
+import logoImg from "../../assets/images/logo.png";
+
+interface PageHeaderProps {
+  title: string;
+}
+
+const PageHeader: React.FC<PageHeaderProps> = ({ title }) => {
+  function handleGoBack() {}
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.topBar}>
+        <BorderlessButton onPress={handleGoBack}>
+          <Image source={backIcon} resizeMode="contain" />
+        </BorderlessButton>
+
+        <Image source={logoImg} resizeMode="contain" />
+      </View>
+
+      <Text style={styles.title}>{title}</Text>
+    </View>
+  );
+};
+
+export default PageHeader;
--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -2,9 +2,14 @@ import React from "react";
 import { View } from "react-native";
 
 import styles from "./styles";
+import PageHeader from "../../components/PageHeader";
 
 const TeacherList: React.FC = () => {
-  return <View style={styles.container} />;
+  return (
+    <View style={styles.container}>
+      <PageHeader />
+    </View>
+  );
 };
 
 export default TeacherList;
