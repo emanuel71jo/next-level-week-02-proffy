b241a961865aecb54df18a109efb5c9ab1f9886c
emanuel71jo <emanuel71jo@gmail.com>
2020-08-08 13:12:25 -0300

    bottons de filtro

--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, ScrollView } from "react-native";
+import { View, ScrollView, Text, TextInput } from "react-native";
 
 import styles from "./styles";
 import PageHeader from "../../components/PageHeader";
@@ -8,7 +8,36 @@ import TeacherItem from "../../components/TeacherItem";
 const TeacherList: React.FC = () => {
   return (
     <View style={styles.container}>
-      <PageHeader title="Proffys disponíveis" />
+      <PageHeader title="Proffys disponíveis">
+        <View style={styles.searchForm}>
+          <Text style={styles.label}>Matéria</Text>
+          <TextInput
+            style={styles.input}
+            placeholderTextColor="#c1bccc"
+            placeholder="Qual a matéria"
+          />
+
+          <View style={styles.inputGroup}>
+            <View style={styles.inputBlock}>
+              <Text style={styles.label}>Dia da semana</Text>
+              <TextInput
+                style={styles.input}
+                placeholderTextColor="#c1bccc"
+                placeholder="Qual o dia?"
+              />
+            </View>
+
+            <View style={styles.inputBlock}>
+              <Text style={styles.label}>Horário</Text>
+              <TextInput
+                style={styles.input}
+                placeholderTextColor="#c1bccc"
+                placeholder="Qual horário?"
+              />
+            </View>
+          </View>
+        </View>
+      </PageHeader>
 
       <ScrollView
         style={styles.teacherList}
